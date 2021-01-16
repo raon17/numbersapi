3ec060b516cd3256c153ec1e332cc713461112f4
William Kim <william.kim171@gmail.com>
2021-01-16 21:30:42 +1100

    Updated HTML and Javascript

--- number.js
+++ number.js
@@ -1,17 +1,38 @@
 let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
 let numberInput = document.querySelector('#numberInput')
-numberInput.addEventListener('input', getFactAjax);
+// numberInput.addEventListener('input', getFactAjax);
+numberInput.addEventListener('input', getFactFetch);
 
+/*
+//Ajax
 function getFactAjax(){
    let number = numberInput.value;
    let xhr = new XMLHttpRequest();
+
    xhr.open('GET', 'http://numbersapi.com/'+number);
 
    xhr.onload = function () {
-      if(this.status == 200 && number!= ''){
-         console.log(this.responseText);
+      if(this.status == 200 && number != ''){
+         fact.style.display = 'block';
+         factText.innerText = this.responseText;
       } 
    }
    xhr.send();
+}
+*/
+
+//Fetch
+function getFactFetch(){
+   let number = numberInput.value;
+
+   fetch('http://numbersapi.com/'+number)
+      .then(response => response.text())
+      .then(data => {
+         if(number != ''){
+            fact.style.display = 'block';
+            factText.innerText = data;
+         }
+      })
+      .catch(err => console.log(err));
 }
