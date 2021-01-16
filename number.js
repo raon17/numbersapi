719afa42f04db88d9ee4382663efbc4c8b64262c
William Kim <william.kim171@gmail.com>
2021-01-16 20:21:24 +1100

    Added number.js

---
+++ number.js
@@ -0,0 +1,17 @@
+let fact = document.querySelector('#fact');
+let factText = document.querySelector('#factText');
+let numberInput = document.querySelector('#numberInput')
+numberInput.addEventListener('input', getFactAjax);
+
+function getFactAjax(){
+   let number = numberInput.value;
+   let xhr = new XMLHttpRequest();
+   xhr.open('GET', 'http://numbersapi.com/'+number);
+
+   xhr.onload = function () {
+      if(this.status == 200 && number!= ''){
+         console.log(this.responseText);
+      } 
+   }
+   xhr.send();
+}
